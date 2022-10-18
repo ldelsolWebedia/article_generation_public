ad18382f486303cd6decce9bd55e0e9cc8f013d4
Louis Delsol <louis.delsol@webedia-group.fr>
2022-10-18 15:07:59 +0200

    Update scraping

--- scraping_senscritique.py
+++ scraping_senscritique.py
@@ -30,7 +30,7 @@ def get_JV_features(entity):
     """
     options = webdriver.ChromeOptions() 
     # options.add_argument("start-maximized")
-    # options.add_argument("headless")
+    options.add_argument("headless")
     # options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')  
     # options.add_argument('--disable-blink-features=AutomationControlled')
